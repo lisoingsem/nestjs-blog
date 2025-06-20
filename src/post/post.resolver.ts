60b56ccd60db64413d11d420f7cc030191631a21
lisoingsem <lisoing2018@gmail.com>
2025-06-20 16:06:48 +0700

    Add PostResolver with CRUD operations for posts

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { PostService } from './post.service';
+import { Post } from './entities/post.entity';
+import { CreatePostInput } from './dto/create-post.input';
 
-@Resolver()
-export class PostResolver {}
+@Resolver(() => Post)
+export class PostResolver {
+  constructor(private readonly postService: PostService) {}
+
+  @Mutation(() => Post)
+  createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
+    return this.postService.create(createPostInput);
+  }
+
+  @Query(() => [Post], { name: 'posts' })
+  findAll() {
+    return this.postService.findAll();
+  }
+
+  @Query(() => Post, { name: 'post' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.postService.findOne(id);
+  }
+}

7b3efcda92f148a239c064c414e99c0a88022d61
lisoingsem <lisoing2018@gmail.com>
2025-06-25 17:22:42 +0700

    Remove soft delete decorator from the shared decorators module, streamlining exports in index.ts.

--- src/shared/decorators/index.ts
+++ src/shared/decorators/index.ts
@@ -1,2 +1,1 @@
-export * from './current-user.decorator';
-export * from './soft-delete.decorator'; 
+export * from './current-user.decorator'; 
--- src/shared/decorators/soft-delete.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const SOFT_DELETE_KEY = 'softDelete';
-export const SoftDelete = () => SetMetadata(SOFT_DELETE_KEY, true); 
